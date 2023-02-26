f705b067dfca45bc1d47df93b2441ab5412aa44f
tomasrembik <tomasrembik@hotmail.com>
2023-02-26 20:49:07 +0100

    Zadanie domowe dzien 4

---
+++ challengeAppp/challengeAppp/ZD 4.cs
@@ -0,0 +1,18 @@
+﻿// zadanko domowe dzien 4
+
+var name = "EWA";
+bool woman = true;
+var age = 33;
+
+if ( woman == false && age < 18 )
+{
+    Console.WriteLine(" Niepełnoletni mężczyzna");
+}
+else if ( woman == true && age < 30)
+{
+    Console.WriteLine(" Kobieta poniżej 30 lat");
+}
+else if ( name == "EWA" && age == 33)
+{
+    Console.WriteLine("EWA, lat 33");
+}
